bb0b37712c5927d9a2f772c43e3f696f5e836b81
Naan Mohammed <naanahmed.na@gmail.com>
2022-08-08 11:54:23 +0300

    Day1 -practices

---
+++ Day 1/Day1-project (Band Name Generator).py
@@ -0,0 +1,6 @@
+
+print("Band Name Generator Project")
+print("---------------------------------------------------------")
+
+print("Hello, " + input("What is your name?") + input("What city did you grow up in? "))
+city_name=input("What city did you grow up in? ")

131f8e337abca440a2e71463f52bd7c3a2dbdb04
Naan Mohammed <naanahmed.na@gmail.com>
2022-08-14 11:46:15 +0300

    Day 2-practices

--- Day 2- Data Types, Operations, F-strings, Type Conversions/Day2-Practices.py
+++ Day 2- Data Types, Operations, F-strings, Type Conversions/Day2-Practices.py
@@ -1,4 +1,59 @@
+###print("Naan"[1])
+###num_char=str(len(input("Enter your name\n")));
+###print("Your name has " + num_char + " characters");
+###print(70 + float("100"));
 
-print("Naan"[1])
-num_char=str(len(input("Enter your name")));
-print("Your name has " + num_char + " characters");
+###print("--------------------------------------\n\n")
+####num_orig=input("Enter a two digit number");
+####print(int(num_orig[0]) + int(num_orig[1]))
+
+###two_digit_number=input("Enter a two digit number \n");
+###first_digit=two_digit_number[0];
+###second_digit=two_digit_number[1];
+###result=int(first_digit) + int(second_digit)
+###print(result);
+
+###print("--------------------------------------\n\n")
+###print(3*3+3/3-3)
+
+##print("--------------------------------------\n\n")
+
+##height=input("Enter your height in meters \n")
+##weight=input("Enter your weight in kilograms\n")
+##h=float(height)
+##w=int(weight)
+##BMI=int(w/h**2);
+##print(BMI)
+
+##print("--------------------------------------\n\n")
+##print(round(8/3,2))
+
+#print("--------------------------------------\n\n")
+#score= 5
+#height=1.8
+#isWinning = True
+#print(f"Your height is {height}, your score is {score}, your isWinning status is {isWinning}");
+
+
+
+print("--------------------------------------\n\n")
+total_months_to_live=1080;
+total_weeks_to_live= 4680;
+total_days_to_live= 32850;
+current_age_in_years=input("Enter your age\n");
+current_age_in_months=int(current_age_in_years) * 12
+current_age_in_weeks=int(current_age_in_years) * 52
+current_age_in_days=int(current_age_in_years) * 365
+remaining_months= total_months_to_live - current_age_in_months
+remaining_weeks= total_weeks_to_live - current_age_in_weeks
+remaining_days= total_days_to_live - current_age_in_days
+print(f"You have {remaining_days} days, {remaining_weeks} weeks, and {remaining_months} months left.");
+
+print("--------------------------------------\n\n")
+
+
+
+
+
+
+
